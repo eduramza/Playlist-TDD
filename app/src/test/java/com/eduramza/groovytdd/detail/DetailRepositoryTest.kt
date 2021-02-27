1362d76293bf9b94e6dda9e36655de1ed5c76279
eduramza <eduardo.rmb@outlook.com>
2021-02-26 20:45:46 -0800

    finish second test case

--- app/src/test/java/com/eduramza/groovytdd/detail/DetailRepositoryTest.kt
+++ app/src/test/java/com/eduramza/groovytdd/detail/DetailRepositoryTest.kt
@@ -1,26 +1,52 @@
 package com.eduramza.groovytdd.detail
 
+import com.eduramza.groovytdd.detail.model.Music
 import com.eduramza.groovytdd.detail.repository.DetailRepository
 import com.eduramza.groovytdd.playlist.service.PlaylistService
+import com.eduramza.groovytdd.utils.BaseUnitTest
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Test
 
 @ExperimentalCoroutinesApi
-class DetailRepositoryTest {
+class DetailRepositoryTest: BaseUnitTest() {
 
-    lateinit var repository: DetailRepository
+    private val successResponse = mock<Music>()
     private val service: PlaylistService = mock()
 
     @Test
     fun getPlaylistFromService() = runBlockingTest {
-        repository = DetailRepository(service)
+        val repository = mockSuccessfulCase()
 
         repository.getMusic()
 
         verify(service).fetchMusic()
 
+    }
+
+    @Test
+    fun emitFlowResultFromService() = runBlockingTest {
+
+        val repository = mockSuccessfulCase()
+
+        repository.getMusic()
+
+        assertEquals(successResponse, repository.getMusic().first().getOrNull())
+    }
+
+    private fun mockSuccessfulCase(): DetailRepository {
+        whenever(service.fetchMusic()).thenReturn(
+            flow {
+                emit(Result.success(successResponse))
+            }
+        )
+
+        return DetailRepository(service)
     }
 }
